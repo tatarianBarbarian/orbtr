88ef8c50c3b174628fb7a17ed194ccb7d7dbbb27
Felix Khafizov <f_khafizov@protonmail.com>
2024-02-13 19:17:58 +0200

    Fix service worker locating

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ import App from './App.vue'
 async function prepare() {
   // @ts-ignore
   const { worker } = await import('./mock/worker')
-  return worker.start()
+  return worker.start({
+    serviceWorker: {
+      // This is useful if your application follows
+      // a strict directory structure.
+      url: import.meta.env.DEV ? '/mockServiceWorker.js' : '/orbtr/mockServiceWorker.js'
+    }
+  })
 }
 
 prepare().then(() => {
