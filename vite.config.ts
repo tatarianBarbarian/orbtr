2db5685882e7063c833dbdb4df1046e0f951abd1
Felix Khafizov <f_khafizov@protonmail.com>
2024-02-13 18:21:32 +0200

    Add public dir to config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
+  publicDir: './public',
   plugins: [vue()],
   resolve: {
     alias: {
